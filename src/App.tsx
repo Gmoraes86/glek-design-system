ac3477d69345a84d3752231cedc4b8763da8ddb5
Geferson de Moraes <geferson.moraes@suno.com.br>
2022-10-08 00:47:07 -0300

    start commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button } from './exports';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Teste</Button>
+    </div>
+  );
+}
+
+export default App;
